22e352a27acb3eab02b6ee65daeb8ea913f6323c
khanh19934 <khanh19934@gmail.com>
2018-01-07 21:17:26 +0700

    fourt commit

--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const socketIO = require('socket.io');
+const http = require('http');
 
+const server = http.createServer(app);
 
 const publicPath = path.join(__dirname,'../client');
+
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+    console.log('new user connected');
+    socket.on('disconnect', () => {
+        console.log('client disconnected');
+    });
+});
 
 app.use(express.static(publicPath));
 const PORT = process.env.NODE_ENV || 8080;
 
-app.listen(PORT,() => {
+server.listen(PORT,() => {
     console.log('Server is running in port: ' + PORT);
 });
